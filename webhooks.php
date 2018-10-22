9798a94fac5f5d5ebdaaaf894bde40c3c0707d1a
chayawooth <chayawooth.puy@gmail.com>
2018-10-23 00:20:11 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'FKoAKVKjCin5Fi0L0/MfjG61C6CyZMUtaV1qtqQBZpSFat1a2YJR6aRdKzFSnHWek9ZSJtoPI8YHYEKePisYwNnzWgHYpU9JNWQ7Fobp/dLykecDXExsjUAqfz5RfgE+2hyI97rpugb+MPBybw/prgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
